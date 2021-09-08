e6a69d47a7ad7b69b98ab63296216a7573063180
Mututwa Mututwa <mututwamututwa59@gmail.com>
2021-09-08 10:21:35 +0200

    Save progress

---
+++ AppUtil.cs
@@ -0,0 +1,25 @@
+﻿//using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcdemo
+{
+    public class AppUtil
+    {
+        public static string className = "AppUtil";
+
+        public static MySqlConnection DBConnection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["REMOTE_DB_CONNECTION"].ConnectionString;
+
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            return conn;
+        }
+
+    }
+}
